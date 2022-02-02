0953f77c05a935ca6776d644381bca18793128e8
CuteAnimeGirl <cnschwarzer@qq.com>
2022-02-02 13:55:01 +0800

    Fix DMS

--- Models/DirectMessageSession.cs
+++ Models/DirectMessageSession.cs
@@ -1,4 +1,6 @@
-﻿namespace QQChannelFramework.Models;
+﻿using Newtonsoft.Json;
+
+namespace QQChannelFramework.Models;
 
 /// <summary>
 /// 私信会话对象
@@ -7,15 +9,18 @@ public class DirectMessageSession {
     /// <summary>
     /// 私信会话关联的频道 id
     /// </summary>
+    [JsonProperty("guild_id")]
     public string GuildId { get; set; }
 
     /// <summary>
     /// 私信会话关联的子频道 id
     /// </summary>
+    [JsonProperty("channel_id")]
     public string ChannelId { get; set; }
 
     /// <summary>
     /// 创建私信会话时间戳
     /// </summary>
+    [JsonProperty("create_time")]
     public DateTime? CreateTime { get; set; }
 }
