7f445a5cb4ee5d63d6b680313f7b4a317d4b3d0c
Chianne1025 <buildcat1025@outlook.com>
2022-04-02 16:07:48 +0800

    fixed

--- Models/MessageModels/RetractMessage.cs
+++ Models/MessageModels/RetractMessage.cs
@@ -5,11 +5,17 @@ namespace QQChannelFramework.Models.MessageModels;
 /// <summary>
 /// 撤回消息模型
 /// </summary>
-public class RetractMessage : Message
+public class RetractMessage
 {
+    /// <summary>
+    /// 撤回的消息
+    /// </summary>
+    [JsonProperty("message")]
+    public Message Message { get; set; }
+
     /// <summary>
     /// 进行撤回操作的用户Id
     /// </summary>
     [JsonProperty("op_user")]
-    public string OperationUserId { get; set; }  
+    public string OperationUserId { get; set; }
 }
