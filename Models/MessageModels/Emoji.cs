38e0dbfd3cd503a3f5c49a90017cb5045267602f
CuteAnimeGirl <cnschwarzer@qq.com>
2022-06-27 22:01:28 +0800

    Emoji

--- Models/MessageModels/Emoji.cs
+++ Models/MessageModels/Emoji.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Newtonsoft.Json;
 
 namespace QQChannelFramework.Models.MessageModels;
@@ -6,17 +7,37 @@ namespace QQChannelFramework.Models.MessageModels;
 /// <summary>
 /// 表情
 /// </summary>
-public class Emoji
-{
-    /// <summary>
-    /// 表情ID
-    /// </summary>
-    [JsonProperty("id")]
-    public string Id { get; set; }
+public class Emoji {
+   /// <summary>
+   /// 表情ID
+   /// </summary>
+   [JsonProperty("id")]
+   public string Id { get; set; }
 
-    /// <summary>
-    /// 表情类型
-    /// </summary>
-    [JsonProperty("type")]
-    public int Type { get; set; }
+   /// <summary>
+   /// 表情类型
+   /// </summary>
+   [JsonProperty("type")]
+   public int Type { get; set; }
+
+   public Emoji() { }
+
+   public Emoji(char emoji) {
+      Type = 2;
+      Id = ((int) emoji).ToString();
+   }
+
+   public static bool operator ==(Emoji lhs, Emoji rhs) {
+      if (ReferenceEquals(lhs, rhs))
+         return true;
+      if (ReferenceEquals(lhs, null))
+         return false;
+      if (ReferenceEquals(rhs, null))
+         return false;
+      return lhs.Id == rhs.Id && lhs.Type == rhs.Type;
+   }
+
+   public static bool operator !=(Emoji lhs, Emoji rhs) {
+      return !(lhs == rhs);
+   }
 }
